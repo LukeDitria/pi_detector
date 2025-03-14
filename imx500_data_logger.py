70e5b49d0f1319d87149d13b5979f06b4f99060a
LukeDitria <luke.ditria@monash.edu>
2025-03-14 13:25:43 +1100

    update the yolo detections format

--- imx500_data_logger.py
+++ imx500_data_logger.py
@@ -98,10 +98,12 @@ class Imx500Logger():
             boxes = np.array_split(boxes, 4, axis=1)
             boxes = zip(*boxes)
 
-        yolo_detections = [(category, box, score)
-                           for box, score, category in zip(boxes, scores, classes)
-                           if score > threshold
-                           ]
+        yolo_detections = []
+        for box, score, category in zip(boxes, scores, classes):
+            if score > threshold:
+                y, x, h, w = box
+                bbox = (float(x), float(y), float(w), float(h))
+                yolo_detections.append((category, bbox, float(score)))
 
         self.detections = [(category, box, score, self.imx500.convert_inference_coords(box, metadata, self.picam2))
             for box, score, category in zip(boxes, scores, classes)
@@ -117,9 +119,6 @@ class Imx500Logger():
             if self.args.save_images:
                 lores_path = os.path.join(self.image_detections_path, filename)
                 cv2.imwrite(lores_path, main)
-
-            print(self.detections)
-            print(yolo_detections)
 
             # Log detections locally
             utils.log_detection(filename, self.json_detections_path, yolo_detections)
