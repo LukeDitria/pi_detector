db8427e515356a4285e0b18dc911ab21dc7bbfa3
LukeDitria <luke.ditria@monash.edu>
2025-03-31 13:24:05 +1100

    create object for using csi cameras with picam2

---
+++ csi_camera.py
@@ -0,0 +1,102 @@
+import logging
+from picamera2 import Picamera2, Preview
+from picamera2.devices import Hailo
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import CircularOutput
+
+import utils
+
+class CameraCSI():
+    def __init__(self, video_wh=(1920,1080), model_wh=(640, 640), fps=5, use_bgr=False,
+                 crop_to_square=False, calibration_file=None, save_video=False, buffer_secs=5,
+                 create_preview=False, rotate_img="none"):
+
+        self.logger = logging.getLogger(__name__)
+        self.logger.info("Camera initialized!")
+
+        self.video_wh = video_wh
+        self.model_wh = model_wh
+        self.fps = fps
+        self.use_bgr = use_bgr
+        self.crop_to_square = crop_to_square
+        self.calibration_file = calibration_file
+        self.save_video = save_video
+        self.buffer_secs = buffer_secs
+        self.create_preview = create_preview
+        self.rotate_img = rotate_img
+
+        # Configure camera streams
+        main_res = {'size': self.video_wh, 'format': 'XRGB8888'}
+
+        if self.use_bgr:
+            lores_format = 'BGR888'
+        else:
+            lores_format = 'RGB888'
+        self.logger.info(f"Low Res format {lores_format}")
+
+        controls = {'FrameRate': self.fps}
+
+        # Keep the aspect ratio of the main image in the lo-res image
+        self.lores_wh = self.model_wh
+        if self.crop_to_square:
+            lores_w = int(round(self.model_wh[0] * (self.video_wh[0] / self.video_wh[1])))
+            self.lores_wh = (lores_w, self.model_wh[1])
+
+        logging.info(f"Low Res video shape HxW: {self.lores_wh[1]}, {self.lores_wh[0]}")
+        lores = {'size': self.lores_wh, 'format': lores_format}
+
+        self.picam2 = Picamera2()
+        config = self.picam2.create_video_configuration(main_res, lores=lores, controls=controls)
+        self.picam2.configure(config)
+
+        cam_params = utils.get_calibration_params(self.calibration_file,
+                                                  self.video_wh,
+                                                  self.lores_wh)
+        if cam_params:
+            self.picam2.pre_callback = lambda req: utils.correct_image(req, cam_params)
+
+        if self.create_preview:
+            self.picam2.start_preview(Preview.QT, x=0, y=0, width=self.video_wh[0], height=self.video_wh[1])
+            self.logger.info(f"Camera Creating Preview")
+
+        self.picam2.start()
+
+        if self.save_video:
+            self.encoder = H264Encoder(1000000, repeat=True)
+            self.encoder.output = CircularOutput(buffersize=self.buffer_secs * self.fps)
+            self.picam2.start_encoder(self.encoder)
+            self.logger.info(f"Saving Video")
+
+
+    def get_frames(self):
+        # Capture and process frame
+        (main_frame, frame), metadata = self.picam2.capture_arrays(["main", "lores"])
+
+        # Resize and crop to model size
+        frame = utils.pre_process_image(frame, rotate=self.rotate_img,
+                                        crop_to_square=self.crop_to_square)
+
+        return main_frame, frame
+
+    def start_video_recording(self, filename):
+        if self.save_video:
+            self.encoder.output.fileoutput = filename
+            self.encoder.output.start()
+        else:
+            self.logger.info("Save video is not running!")
+
+    def stop_video_recording(self):
+        if self.save_video:
+            self.encoder.output.stop()
+        else:
+            self.logger.info("Save video is not running!")
+
+def main():
+    camera = CameraCSI(video_wh=(1920,1080), model_wh=(640, 640), fps=30, create_preview=True)
+
+    while True :
+        main_frame, frame = camera.get_frames()
+
+
+if __name__ == "__main__":
+    main()
